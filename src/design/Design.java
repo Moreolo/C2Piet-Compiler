a557ac701159464a0875fe43ad3ddb33ee46ea45
Moreolo <coolrizzy100@gmail.com>
2024-04-18 10:31:42 +0200

    Hinzufügen der parse Methode für die Schnittstelle

--- src/design/Design.java
+++ src/design/Design.java
@@ -1,5 +1,10 @@
 package design;
 
+import java.util.LinkedList;
+import piet.datatypes.*;
+
 public class Design {
-    
+    public static void parse(LinkedList<Block> blocks) {
+        
+    }
 }
---
+++ src/piet/Piet.java
@@ -0,0 +1,10 @@
+package piet;
+
+import java.util.LinkedList;
+import piet.datatypes.Block;
+
+public class Piet {
+    public static LinkedList<Block> parse() {
+        return new LinkedList<Block>(); //Diese Zeile verhindert nur die Fehlermeldung
+    }
+}
