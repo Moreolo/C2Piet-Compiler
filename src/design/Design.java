84d599fcfa5baf276750ccf198ce1ecad964fd6f
Fledermaunz <94017090+Fledermaunz@users.noreply.github.com>
2024-04-22 09:10:11 +0200

    kleiner Commit-Test

--- src/design/Design.java
+++ src/design/Design.java
@@ -5,6 +5,61 @@ import piet.datatypes.*;
 
 public class Design {
     public static void parse(LinkedList<Block> blocks) {
-        
+
     }
+
+    // test
+    /*
+     * private void processBlock(){
+     * 
+     * 
+     * switch (operation.get("title").getAsString()) {
+     * case ("blue"):
+     * currentMatrix = matrixOfColor[4][1];
+     * currentColor = 4;
+     * currentShade = 1;
+     * image.setRGB(5, height - 1, currentMatrix);
+     * break;
+     * 
+     * case ("lightYellow"):
+     * currentMatrix = matrixOfColor[1][0];
+     * currentColor = 1;
+     * currentShade = 0;
+     * image.setRGB(5, height - 2, currentMatrix);
+     * break;
+     * 
+     * case ("darkCyan"):
+     * currentMatrix = matrixOfColor[3][2];
+     * currentColor = 3;
+     * currentShade = 2;
+     * image.setRGB(5, height - 3, currentMatrix);
+     * break;
+     * 
+     * case ("darkGreen"):
+     * currentMatrix = matrixOfColor[2][2];
+     * currentColor = 2;
+     * currentShade = 2;
+     * image.setRGB(5, height - 6, currentMatrix);
+     * break;
+     * case ("int"):
+     * currentMatrix = matrixOfColor[(currentColor + 1) %
+     * matrixOfColor.length][currentShade];
+     * currentColor += 1;
+     * for (int i = 1; i <= (operation.get("count").getAsInt()); i++) {
+     * image.setRGB((5 + i), height - 4, currentMatrix);
+     * }
+     * break;
+     * 
+     * case ("push"):
+     * 
+     * currentMatrix = matrixOfColor[currentColor][(currentShade + 1) % 3];
+     * currentShade += 1;
+     * image.setRGB(11, height - 4, currentMatrix);
+     * break;
+     * 
+     * default:
+     * break;
+     * }
+     * 
+     */
 }
