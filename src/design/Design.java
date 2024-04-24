d845a5dc948bbd73c496656ffd44b55ac6a384c7
Fledermaunz <94017090+Fledermaunz@users.noreply.github.com>
2024-04-24 09:46:59 +0200

    Farben und Switch
    Farben definiert und in einem Array, Switch-Cases grobe Struktur

--- src/design/Design.java
+++ src/design/Design.java
@@ -1,65 +1,146 @@
 package design;
 
 import java.util.LinkedList;
+
 import piet.datatypes.*;
 
 public class Design {
-    public static void parse(LinkedList<Block> blocks) {
 
+    // Farben
+    // Rot
+    int red = (255 << 16) | (0 << 8) | 0;
+    int lightRed = (255 << 16) | (192 << 8) | 192;
+    int darkRed = (192 << 16) | (0 << 8) | 0;
+
+    // Gelb
+    int yellow = (255 << 16) | (255 << 8) | 0;
+    int lightYellow = (255 << 16) | (255 << 8) | 192;
+    int darkYellow = (192 << 16) | (192 << 8) | 0;
+
+    // Grün
+    int green = (0 << 16) | (255 << 8) | 0;
+    int lightGreen = (192 << 16) | (255 << 8) | 192;
+    int darkGreen = (0 << 16) | (192 << 8) | 0;
+
+    // Cyan
+    int cyan = (0 << 16) | (255 << 8) | 255;
+    int lightCyan = (192 << 16) | (255 << 8) | 255;
+    int darkCyan = (0 << 16) | (192 << 8) | 192;
+
+    // Blau
+    int blue = (0 << 16) | (0 << 8) | 255;
+    int lightBlue = (192 << 16) | (192 << 8) | 255;
+    int darkBlue = (0 << 16) | (0 << 8) | 192;
+
+    // Magenta
+    int magenta = (255 << 16) | (0 << 8) | 255;
+    int lightMagenta = (255 << 16) | (192 << 8) | 255;
+    int darkMagenta = (192 << 16) | (0 << 8) | 192;
+
+    // Weiß
+    int white = (255 << 16) | (255 << 8) | 255;
+
+    // Schwarz
+    int black = (0 << 16) | (0 << 8) | 0;
+
+    // verschachteltes FarbenArray
+    int[][] matrixOfColor = {
+            { lightRed, red, darkRed },
+            { lightYellow, yellow, darkYellow },
+            { lightGreen, green, darkGreen },
+            { lightCyan, cyan, darkCyan },
+            { lightBlue, blue, darkBlue },
+            { lightMagenta, magenta, darkMagenta },
+
+    };
+
+    LinkedList<Operation> ops = new LinkedList<Operation>();
+
+    public static void parse(LinkedList<Block> blocks) {
     }
 
-    // test
-    /*
-     * private void processBlock(){
-     * 
-     * 
-     * switch (operation.get("title").getAsString()) {
-     * case ("blue"):
-     * currentMatrix = matrixOfColor[4][1];
-     * currentColor = 4;
-     * currentShade = 1;
-     * image.setRGB(5, height - 1, currentMatrix);
-     * break;
-     * 
-     * case ("lightYellow"):
-     * currentMatrix = matrixOfColor[1][0];
-     * currentColor = 1;
-     * currentShade = 0;
-     * image.setRGB(5, height - 2, currentMatrix);
-     * break;
-     * 
-     * case ("darkCyan"):
-     * currentMatrix = matrixOfColor[3][2];
-     * currentColor = 3;
-     * currentShade = 2;
-     * image.setRGB(5, height - 3, currentMatrix);
-     * break;
-     * 
-     * case ("darkGreen"):
-     * currentMatrix = matrixOfColor[2][2];
-     * currentColor = 2;
-     * currentShade = 2;
-     * image.setRGB(5, height - 6, currentMatrix);
-     * break;
-     * case ("int"):
-     * currentMatrix = matrixOfColor[(currentColor + 1) %
-     * matrixOfColor.length][currentShade];
-     * currentColor += 1;
-     * for (int i = 1; i <= (operation.get("count").getAsInt()); i++) {
-     * image.setRGB((5 + i), height - 4, currentMatrix);
-     * }
-     * break;
-     * 
-     * case ("push"):
-     * 
-     * currentMatrix = matrixOfColor[currentColor][(currentShade + 1) % 3];
-     * currentShade += 1;
-     * image.setRGB(11, height - 4, currentMatrix);
-     * break;
-     * 
-     * default:
-     * break;
-     * }
-     * 
-     */
+    private void opIter() {
+        for (Operation name : ops) {
+            processOperation(name);
+        }
+        ;
+    }
+
+    private void processOperation(Operation operation) {
+        switch (operation.getName()) {
+            case ("color"):
+
+                break;
+            case ("push"):
+
+                break;
+
+            case ("pop"):
+
+                break;
+
+            case ("add"):
+
+                break;
+
+            case ("subtract"):
+
+                break;
+
+            case ("multiply"):
+
+                break;
+
+            case ("divide"):
+
+                break;
+            case ("mod"):
+
+                break;
+
+            case ("not"):
+
+                break;
+
+            case ("greater"):
+
+                break;
+
+            case ("pointer"):
+
+                break;
+
+            case ("switch"):
+
+                break;
+
+            case ("duplicate"):
+
+                break;
+
+            case ("roll"):
+
+                break;
+
+            case ("inNumber"):
+
+                break;
+
+            case ("inChar"):
+
+                break;
+
+            case ("outNumber"):
+
+                break;
+
+            case ("outChar"):
+
+                break;
+
+            default:
+                break;
+        }
+    }
+
 }
