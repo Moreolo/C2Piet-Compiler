8f9e3c2d557e784320d219a29d7e8c17e18c958d
Schmarius <mas4859@thi.de>
2024-05-08 13:22:38 +0200

    Adding Piet.java first changes

--- src/piet/Piet.java
+++ src/piet/Piet.java
@@ -1,10 +1,34 @@
 package piet;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+
+import basicblocks.datatypes.*;
 import piet.datatypes.Block;
 
 public class Piet {
-    public static LinkedList<Block> parse() {
-        return new LinkedList<Block>(); //Diese Zeile verhindert nur die Fehlermeldung
+
+    LinkedList<Integer> VariablenSpeicher = new LinkedList<>();
+
+    int ProgramCounter = 0;
+
+    public static LinkedList<Block> parse(ArrayList<BBlock> blocks) {
+        for (BBlock block : blocks) {
+            if (block instanceof CondBlock) parseCondition(block);
+            if (block instanceof FuncBlock) parseFunction(block);
+            if (block instanceof TermBlock) parseTerm(block);
+        }
+    }
+
+    private Block parseCondition(Block block){
+
+    }
+    
+    private Block parseFunction(Block block){
+
+    }
+    
+    private Block parseTerm(Block block){
+
     }
 }
