4d444dd78880ec0f7a5a3bc3309eb849540a0abd
Jo-sua <jos3693@thi.de>
2024-06-13 13:48:40 +0200

    Erstellen eines Function DefinitionBasic Blocks, der die Parameternamen einer Funktion enthält
    Co-authored-by: S4NT1-1 <82910836+S4NT1-1@users.noreply.github.com>

---
+++ src/basicblocks/datatypes/FunDefBlock.java
@@ -0,0 +1,16 @@
+package basicblocks.datatypes;
+
+import java.util.ArrayList;
+
+import ast.datatypes.Node;
+
+public class FunDefBlock extends BBlock{
+    public FunDefBlock(int positionInArray) {
+        super(positionInArray);
+        
+    }
+    ArrayList<Node> parameterList;
+
+
+
+}
